39a6c9ff28e0b38598c25bdc5a09a14c1c6730fb
TAMIL <tamil323111@gmail.com>
2026-04-01 13:12:46 +0530

    Add files via upload

--- count number.py
+++ count number.py
@@ -1,6 +1,6 @@
+count=0
 num = int(input("Enter a number: ")) 
-count = 0 
 while num > 0: 
   count += 1 
   num = num // 10 
-print("Total digits:", count) 
+print( count) 
--- factorial.py
+++
@@ -1,9 +0,0 @@
-# function to find factorial of given number
-def factorial(n):
- if (n==1 or n==0):
-     return 1 
- else:
-     return n * factorial(n_1) #recursion
- 
-num = 6;
-print("Factorial of,num,"is",factorial(num)")
